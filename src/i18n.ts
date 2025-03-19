673e81c7052639338003aea5a1d6b7a244100633
Lee Goddard <leegee@gmail.com>
2025-03-19 08:50:23 +0100

    Simpler

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,4 @@
 import i18n from "i18next";
-import { createSignal, createEffect } from "solid-js";
-import { createStore } from "solid-js/store";
 import appConfig from "../app.config.json";
 
 const { defaultLanguage, targetLanguage, uiLanguage, i18n: i18nConfig } = appConfig;
@@ -19,16 +17,4 @@ i18n.init({
     interpolation: { escapeValue: false },
 });
 
-export const useTranslation = () => {
-    const [t, setT] = createSignal(i18n.t);
-    const [store, setStore] = createStore({ t: i18n.t });
-
-    createEffect(() => {
-        i18n.on("languageChanged", () => {
-            setT(() => i18n.t);
-            setStore("t", i18n.t);
-        });
-    });
-
-    return [store.t, i18n.changeLanguage];
-};
+export const t = i18n.t; 
