6d261f744bd830ab82920716b53b1e612c9abc30
Lee Goddard <leegee@gmail.com>
2025-03-21 10:07:46 +0100

    Remove 'skip' from ActionButton

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,31 +1,15 @@
-import { createSignal, createEffect } from 'solid-js';
 import { t } from '../i18n';
 import './ActionButton.css';
 
 interface ButtonProps {
     isCorrect: boolean | null;
     isInputPresent: boolean;
-    minClickBeforeSkip?: number;
     onCheckAnswer: () => void;
     onComplete: () => void;
 }
 
 const ActionButton = (props: ButtonProps) => {
-    const [clicksCounted, setClicksCounted] = createSignal(0);
-
-    props.minClickBeforeSkip ||= 4;
-
-    console.log("Rendering ActionButton, isInputPresent:", props.isInputPresent);
-
-    // Reset the clicksCounted signal when isCorrect changes
-    createEffect(() => {
-        setClicksCounted(0);
-    });
-
     const handleClick = () => {
-        console.log('actionbutton handleclick enter', props.isCorrect);
-        setClicksCounted((prev) => prev + 1);
-
         if (props.isCorrect === true) {
             console.log('actionbutton handleclick isCorrect, onComplete');
             props.onComplete();
@@ -36,29 +20,23 @@ const ActionButton = (props: ButtonProps) => {
     };
 
     return (
-        <>
-            <button
-                class={
-                    props.isCorrect === null
-                        ? 'next-button'
-                        : props.isCorrect === true
-                            ? 'correct-next-button'
-                            : 'try-again-button'
-                }
-                onClick={handleClick}
-                disabled={!props.isInputPresent}
-            >
-                {props.isCorrect === null
-                    ? t('check')
+        <button
+            class={
+                props.isCorrect === null
+                    ? 'next-button'
                     : props.isCorrect === true
-                        ? t('correct_next')
-                        : t('try_again')}
-            </button>
-
-            {!props.isCorrect && clicksCounted() >= (props.minClickBeforeSkip) && (
-                <button class='skip-button' onClick={props.onComplete}>{t('skip')}</button>
-            )}
-        </>
+                        ? 'correct-next-button'
+                        : 'try-again-button'
+            }
+            onClick={handleClick}
+            disabled={!props.isInputPresent}
+        >
+            {props.isCorrect === null
+                ? t('check')
+                : props.isCorrect === true
+                    ? t('correct_next')
+                    : t('try_again')}
+        </button>
     );
 };
 
