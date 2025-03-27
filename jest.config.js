2a8478835ae5b84250dfa3974e1ef298e7b61c38
Lee Goddard <leegee@gmail.com>
2025-03-27 11:25:58 +0100

    Don't read non-script files

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,9 @@ export default {
     testEnvironment: "jsdom",
     transform: {
         "^.+\\.(ts|tsx)$": "babel-jest",
+        "^.+\\.(css|less|sass|scss)$": "jest-transform-stub"
     },
     moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
     setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
+
