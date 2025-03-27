5d206576d36d0b153138fd6b58868659b10bcc41
Lee Goddard <leegee@gmail.com>
2025-03-27 11:09:41 +0100

    Drop old vitest config

--- src/setupTests.ts
+++
@@ -1,6 +0,0 @@
-import '@testing-library/jest-dom';
-import "localstorage-memory";
-
-global.window = globalThis as any;
-global.document = globalThis.document || {};
-global.localStorage = localStorage;
