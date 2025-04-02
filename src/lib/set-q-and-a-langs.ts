23d54a2925ba83e4280af935eff85d912d5a8073
Lee Goddard <leegee@gmail.com>
2025-04-02 09:36:13 +0200

    Some cards may not require translations

--- src/lib/set-q-and-a-langs.ts
+++ src/lib/set-q-and-a-langs.ts
@@ -4,6 +4,12 @@ import config from '../config';
 export type setQandALangsReturnType = { q: string, a: string };
 
 export const setQandALangs = (card: IBaseCard): setQandALangsReturnType => {
+    if (card.alang && card.alang === card.qlang) {
+        return {
+            q: card.qlang,
+            a: card.alang
+        };
+    }
     return {
         q: (card.qlang === 'default' ? config.defaultLanguage : config.targetLanguage) as string,
         a: (card.alang ?? (card.qlang === 'default' ? config.targetLanguage : config.defaultLanguage)) as string,
