3eb74dee122209ac01ed5139d9c0d779bddf0469
Lee Goddard <leegee@gmail.com>
2025-04-02 09:18:52 +0200

    Casts

--- src/global-state/lessons.ts
+++ src/global-state/lessons.ts
@@ -4,27 +4,27 @@ interface Answers {
 
 const STORAGE_PREFIX = 'oe_';
 
-const storageKeys = {
+export const storageKeys = {
   CURRENT_LESSON_INDEX: (courseId: number) => `${STORAGE_PREFIX}current_lesson_index_${courseId}`,
   ANSWERS: (courseId: number) => `${STORAGE_PREFIX}answers_${courseId}`,
   COURSE_INDEX: `${STORAGE_PREFIX}course`,
 };
 
-let courseId: number = getCourseIndex();
+export let Current_Course_Index: number = getCourseIndex();
 
 export const setCurrentLessonIndex = (lessonIndex: number) => {
-  localStorage.setItem(storageKeys.CURRENT_LESSON_INDEX(courseId), String(lessonIndex));
+  localStorage.setItem(storageKeys.CURRENT_LESSON_INDEX(Current_Course_Index), String(lessonIndex));
 };
 
 export const getCurrentLessonIndex = () => {
-  return JSON.parse(localStorage.getItem(storageKeys.CURRENT_LESSON_INDEX(courseId)) || '0');
+  return JSON.parse(localStorage.getItem(storageKeys.CURRENT_LESSON_INDEX(Current_Course_Index)) || '0');
 };
 
 export const getLessonAnswers = (lessonIndex: number): string[][] => {
   let answers: Answers = {};
 
   try {
-    answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(courseId)) || "{}");
+    answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(Current_Course_Index)) || "{}");
   } catch (error) {
     console.error("Error parsing lesson answers:", error);
   }
@@ -33,7 +33,10 @@ export const getLessonAnswers = (lessonIndex: number): string[][] => {
 };
 
 export const saveAnswer = (lessonIndex: number, cardIndex: number, incorrectAnswer: string = ""): void => {
-  const savedAnswers: Answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(courseId)) || '{}');
+  const savedAnswers: Answers = JSON.parse(
+    localStorage.getItem(storageKeys.ANSWERS(Current_Course_Index))
+    || '{}'
+  );
   savedAnswers[lessonIndex] ??= [];
 
   if (!savedAnswers[lessonIndex][cardIndex]) {
@@ -44,30 +47,30 @@ export const saveAnswer = (lessonIndex: number, cardIndex: number, incorrectAnsw
 
   savedAnswers[lessonIndex][cardIndex].push(incorrectAnswer);
 
-  localStorage.setItem(storageKeys.ANSWERS(courseId), JSON.stringify(savedAnswers));
+  localStorage.setItem(storageKeys.ANSWERS(Current_Course_Index), JSON.stringify(savedAnswers));
 };
 
 export const resetCourse = () => {
-  localStorage.removeItem(storageKeys.CURRENT_LESSON_INDEX(courseId));
-  localStorage.removeItem(storageKeys.ANSWERS(courseId));
+  localStorage.removeItem(storageKeys.CURRENT_LESSON_INDEX(Current_Course_Index));
+  localStorage.removeItem(storageKeys.ANSWERS(Current_Course_Index));
   setCurrentLessonIndex(0);
 };
 
 export const resetLesson = (lessonIndex: number): void => {
-  const savedAnswers: Answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(courseId)) || '{}');
+  const savedAnswers: Answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(Current_Course_Index)) || '{}');
   if (savedAnswers[lessonIndex]) {
     savedAnswers[lessonIndex] = [];
-    localStorage.setItem(storageKeys.ANSWERS(courseId), JSON.stringify(savedAnswers));
+    localStorage.setItem(storageKeys.ANSWERS(Current_Course_Index), JSON.stringify(savedAnswers));
   }
 };
 
 export const getTotalTakenLessons = (): number => {
-  const answers: Answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(courseId)) || '{}');
+  const answers: Answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(Current_Course_Index)) || '{}');
   return Object.keys(answers).length;
 };
 
 export const getTotalQuestionsAnswered = (): number => {
-  const answers: Answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(courseId)) || '{}');
+  const answers: Answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(Current_Course_Index)) || '{}');
   return Object.values(answers).reduce(
     (total: number, lessonAnswers: string[][]) =>
       total + (Array.isArray(lessonAnswers) ? lessonAnswers.length : 0),
@@ -76,7 +79,7 @@ export const getTotalQuestionsAnswered = (): number => {
 };
 
 export const getTotalCorrectAnswers = (): number => {
-  const answers: Answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(courseId)) || '{}');
+  const answers: Answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(Current_Course_Index)) || '{}');
   return Object.values(answers).reduce(
     (total: number, lessonAnswers: string[][]) =>
       total + (Array.isArray(lessonAnswers) ? lessonAnswers.reduce((sum: number, cardAnswers: string[]) => sum + (Array.isArray(cardAnswers) ? cardAnswers.filter(answer => answer === "").length : 0), 0) : 0),
@@ -85,7 +88,7 @@ export const getTotalCorrectAnswers = (): number => {
 };
 
 export const getTotalIncorrectAnswers = (): number => {
-  const answers: Answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(courseId)) || '{}');
+  const answers: Answers = JSON.parse(localStorage.getItem(storageKeys.ANSWERS(Current_Course_Index)) || '{}');
   return Object.values(answers).reduce(
     (total: number, lessonAnswers: string[][]) =>
       total + (Array.isArray(lessonAnswers) ? lessonAnswers.reduce((sum: number, cardAnswers: string[]) => sum + (Array.isArray(cardAnswers) ? cardAnswers.filter(answer => answer !== "").length : 0), 0) : 0),
@@ -115,11 +118,11 @@ export const getLessonQuestionsAnsweredCorrectly = (lessonIndex: number): number
 };
 
 export function getCourseIndex() {
-  const savedIndex = localStorage.getItem(storageKeys.COURSE_INDEX);
-  return savedIndex === null ? 0 : parseInt(savedIndex, 10);
+  const savedIndex = Number(localStorage.getItem(storageKeys.COURSE_INDEX));
+  return isNaN(savedIndex) ? 0 : savedIndex;
 };
 
 export const setCourseIndex = (index: number) => {
-  courseId = index;
+  Current_Course_Index = Number(index);
   localStorage.setItem(storageKeys.COURSE_INDEX, index.toString());
 };
