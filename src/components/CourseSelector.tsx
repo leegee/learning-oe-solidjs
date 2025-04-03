773e2be65f5a731bba1d3e90e93ffa8f67abbe16
Lee Goddard <leegee@gmail.com>
2025-04-03 16:11:22 +0200

    use config context

---
+++ src/components/CourseSelector.tsx
@@ -0,0 +1,19 @@
+import { courseStore, setSelectedCourse } from "../global-state/course";
+import { useConfigContext } from '../contexts/Config';
+
+const CourseSelector = () => {
+    const { config } = useConfigContext();
+    return (
+        <>
+            {config.lessons.map((course, index) => (
+                <li class={courseStore.selectedCourseIndex === index ? 'selected' : ''}>
+                    <button onClick={() => setSelectedCourse(index)}>
+                        {course.title}
+                    </button>
+                </li>
+            ))}
+        </>
+    );
+};
+
+export default CourseSelector;
