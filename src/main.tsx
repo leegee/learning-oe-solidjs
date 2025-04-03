fd3c42221b1db70d07c6d3c655400c028d475cc3
Lee Goddard <leegee@gmail.com>
2025-04-03 11:27:00 +0200

    Load config before starting the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
+import { createRoot } from 'solid-js';
 import { render } from "solid-js/web";
+
 import App from "./App";
+import { loadConfig } from './config';
 import "./i18n";
 import "./global-css/index.css";
 
-const root = document.getElementById("root");
+const initializeAppConfig = async () => {
+  try {
+    const config = await loadConfig();
+    return config;
+  } catch (error) {
+    console.error("Error loading configuration:", error);
+    return null;
+  }
+};
 
-if (root) {
-  render(() => <App />, root);
-}
+const startApp = async () => {
+  const appConfig = await initializeAppConfig();
+
+  if (appConfig) {
+    const root = document.getElementById("root");
+
+    if (root) {
+      render(() => <App />, root);
+    } else {
+      createRoot(() => (<App />));
+    }
+
+  } else {
+    console.error("Failed to load config. App will not start.");
+  }
+};
+
+startApp(); 
