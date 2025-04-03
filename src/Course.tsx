dff7e7ccefff98fbd2bf3f28889f67dc2d68d7d0
Lee Goddard <leegee@gmail.com>
2025-04-03 16:07:04 +0200

    use config context

--- src/Course.tsx
+++ src/Course.tsx
@@ -1,12 +1,12 @@
 import { courseStore, setSelectedCourse } from "./global-state/course";
-import { loadConfig } from "./config";
+import { useConfigContext } from "./contexts/Config";
 
-const appConfig = await loadConfig();
 
 const CourseSelector = () => {
+    const { config } = useConfigContext();
     return (
         <>
-            {appConfig.lessons.map((course, index) => (
+            {config.lessons.map((course, index) => (
                 <li class={courseStore.selectedCourseIndex === index ? 'selected' : ''}>
                     <button onClick={() => setSelectedCourse(index)}>
                         {course.title}
