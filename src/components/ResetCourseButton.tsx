79f3eeb7b822f495902482b1e8206702e5df0bab
Lee Goddard <leegee@gmail.com>
2025-04-21 20:14:04 +0200

    useResource

--- src/components/ResetCourseButton.tsx
+++ src/components/ResetCourseButton.tsx
@@ -1,20 +1,21 @@
 /* ResetAllButton */
 
-import { createMemo } from "solid-js";
+import { createMemo, createResource } from "solid-js";
 import { useLessonStore } from "../global-state/lessons";
 import { useConfirm } from "../contexts/ConfirmProvider";
 import { useI18n } from "../contexts/I18nProvider";
-import { useCourseStore } from "../global-state/course";
+import { useCourseStore, type ICourseStore } from "../global-state/course";
 
 export const ResetCourseButtonComponent = () => {
-    const courseStore = useCourseStore();
+    const [courseStore] = createResource<ICourseStore>(() => useCourseStore());
     const { t } = useI18n();
     const lessonStore = useLessonStore();
     const { showConfirm } = useConfirm();
-    const totalAnswered = createMemo(() => lessonStore.getTotalQuestionsAnswered());
+    const totalAnswered = createMemo(() => lessonStore!.getTotalQuestionsAnswered());
 
     const onConfirmed = () => {
-        courseStore.reset();
+        if (courseStore.loading) return;
+        courseStore()!.reset();
         window.location.reload();
     }
 
