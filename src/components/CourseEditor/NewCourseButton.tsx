8869bc5ba4960b7e4a4e2f57ea33c24d42b84523
Lee Goddard <leegee@gmail.com>
2025-04-28 11:50:17 +0200

    Route to create new course

--- src/components/CourseEditor/NewCourseButton.tsx
+++ src/components/CourseEditor/NewCourseButton.tsx
@@ -1,24 +1,11 @@
-import { createResource } from "solid-js";
-import { useParams, useNavigate } from "@solidjs/router";
-import { useConfigContext } from "../../contexts/ConfigProvider";
-import { ICourseStore, useCourseStore } from "../../global-state/course";
+import { useNavigate } from "@solidjs/router";
+import { RouteFragementInitCourse } from "../../Routes";
 
 const NewCourseButton = () => {
     const navigate = useNavigate();
-    const { config } = useConfigContext();
-    const [courseStore] = createResource<ICourseStore>(useCourseStore);
-    const params = useParams();
 
     const handlClick = async () => {
-
-        if (courseStore.loading) return;
-        const courseIdx = params.courseIdx;
-        if (!courseIdx) return;
-
-        const newIdx = courseStore()?.initNewCourse(config);
-        navigate(`/editor/${newIdx}`);
-        return;
-
+        return navigate(`/editor/${RouteFragementInitCourse}`);
     }
 
     return (
