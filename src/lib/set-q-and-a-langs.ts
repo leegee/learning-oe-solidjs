a1d73be5f311928a5045a2eff28ab096742b1d56
Lee Goddard <leegee@gmail.com>
2025-06-17 23:39:12 +0200

    Decruft

--- src/lib/set-q-and-a-langs.ts
+++ src/lib/set-q-and-a-langs.ts
@@ -1,5 +1,4 @@
 import { IBaseCard } from '../components/Cards/BaseCard.type';
-// import { useConfigContext } from '../contexts/ConfigProvider';
 import { useCourseStore } from '../global-state/course';
 
 export type setQandALangsReturnType = { q: string, a: string };
@@ -9,7 +8,6 @@ export const setQandALangs = async (card: IBaseCard): Promise<setQandALangsRetur
     const sourceLang = courseStore.getSourceLang();
     const targetLang = courseStore.getTargetLang();
 
-    // const { config } = useConfigContext();
     if (card.alang && card.alang === card.qlang) {
         return {
             q: card.qlang,
@@ -17,9 +15,6 @@ export const setQandALangs = async (card: IBaseCard): Promise<setQandALangsRetur
         };
     }
     return {
-        // q: (card.qlang === 'default' ? config.defaultLanguage : config.targetLanguage) as string,
-        // a: (card.alang ?? (card.qlang === 'default' ? config.targetLanguage : config.defaultLanguage)) as string,
-
         q: (card.qlang === 'default' ? sourceLang : targetLang) as string,
         a: (card.alang ?? (card.qlang === 'default' ? targetLang : sourceLang)) as string,
     };
