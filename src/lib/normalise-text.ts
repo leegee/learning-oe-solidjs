2e27a6e165dbae6ef7f2f6b5932147d8475e1e7e
Lee Goddard <leegee@gmail.com>
2025-07-29 16:42:34 +0200

    preserve whitespace but normalise

--- src/lib/normalise-text.ts
+++ src/lib/normalise-text.ts
@@ -1,5 +1,6 @@
 export const normalizeText = (text: string): string => {
     return text.trim().toLocaleLowerCase()
-        .replace(/[^\p{L}\p{N}]+/gu, '')
+        // .replace(/[^\p{L}\p{N}]+/gu, '')
+        .replace(/[^\p{L}\p{N}\s]+/gu, '')
         .replace(/\s+/g, ' ');
 };
