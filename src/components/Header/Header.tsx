68ac065433baece5285f03a32c4072ac6c404ef3
Lee Goddard <leegee@gmail.com>
2025-08-08 10:31:54 +0200

    Don't show 'close menu' when no  course selected

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,25 @@
 import './Header.css';
 import { Show } from 'solid-js';
+import { useParams } from "@solidjs/router";
 import { getCourseStore } from '../../global-state/course';
 import MenuTogglebutton from "../Menu/MenuToggleButton";
 import { useConfigContext } from "../../contexts/ConfigProvider";
 
 const Header = () => {
+    const params = useParams();
     const { config } = useConfigContext();
     const courseStore = getCourseStore();
+
+    const hasCourse = !!params.courseIdx;
 
     return (
         <Show when={courseStore} fallback={<p>Loading...</p>}>
             <header class='header-component'>
                 <div class="header-text">
                     <h1 lang={config.targetLanguage}>{config.appTitle}</h1>
-                    <MenuTogglebutton />
+                    <Show when={hasCourse}>
+                        <MenuTogglebutton />
+                    </Show>
                 </div>
             </header>
         </Show>
