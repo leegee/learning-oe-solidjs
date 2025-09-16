4a5d45ad790a4f4e92a702c176e5058b014a8bc6
Lee Goddard <leegee@gmail.com>
2025-09-16 12:19:33 +0200

    simpler

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -1,14 +1,11 @@
 import { A } from "@solidjs/router";
-import { I18nProvider } from "../contexts/I18nProvider";
 
 export default function Homepage() {
     return (
-        <I18nProvider>
-            <main>
-                <h1>Home Page</h1>
+        <main>
+            <h1>Home Page</h1>
 
-                <A href="/course/1">Course</A>
-            </main>
-        </I18nProvider>
+            <A href="/course/1">Course</A>
+        </main>
     )
 }
